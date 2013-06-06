544deb0b3ad688f523a97e8b0f33ea40322c0ada
琚雪 <juxue.cy@taobao.com>
2013-06-06 10:04:35 +0800

    change

--- _plugins/sitemap_generator.rb
+++ _plugins/sitemap_generator.rb
@@ -40,7 +40,7 @@ require 'rexml/document'
 module Jekyll
 
   # Change MY_URL to reflect the site you are using
-  MY_URL = "http://fanadir.in"
+  MY_URL = "http://juxuechen.github.io"
 
   # Change SITEMAP_FILE_NAME if you would like your sitemap file
   # to be called something else
--- js/juxuechen.github.io.js
+++ js/juxuechen.github.io.js
@@ -1,6 +1,6 @@
 ("*").each(function () { 
    if ($(this).children().length == 0) {
-      var newHTML = $(this).html().replace('(f)','<img src = "../images/fanadir.in.png" />');
+      var newHTML = $(this).html().replace('(f)','<img src = "../images/juxuechen.github.io.png" />');
       $(this).html(newHTML);
    } 
 });

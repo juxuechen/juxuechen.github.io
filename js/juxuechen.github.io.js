29ac81dcf882bdee29060395fbaaa22e03f73908
琚雪 <juxue.cy@taobao.com>
2013-07-31 19:45:02 +0800

    替换

--- js/juxuechen.github.io.js
+++
@@ -1,6 +0,0 @@
-("*").each(function () { 
-   if ($(this).children().length == 0) {
-      var newHTML = $(this).html().replace('(f)','<img src = "../images/juxuechen.github.io.png" />');
-      $(this).html(newHTML);
-   } 
-});
